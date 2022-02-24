0b90df71cefcbe10b35c49e9e0154027ae0a737f
Marygrace <ma.grasso1999@gmail.com>
2022-02-24 16:48:42 +0100

    backup

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function startDraw(){
+  let choice = prompt("Select a grid size, from 2 to 100");
+  numericalchoice = parseInt (choice, 10);
+let container = document.getElementById("container");
+  for (i = 1; i = (numericalchoice * numericalchoice); i++){
+   let box = document.createElement("div");
+   box.classList.add = ("children");
+   container.appendChild(box);
+  }
+
+}
+
+
+
+
+
