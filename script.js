9f389438d042a1cae4c9d02e6570b007d0047a69
Marygrace <ma.grasso1999@gmail.com>
2022-02-25 01:42:39 +0100

    version1.0

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
+let click = true;
 
-function startDraw(){
+function startDraw()
+{
+
   let choice = prompt("Select a grid size, from 2 to 100");
-  numericalchoice = parseInt (choice, 10);
-let container = document.getElementById("container");
-  for (i = 1; i = (numericalchoice * numericalchoice); i++){
-   let box = document.createElement("div");
-   box.classList.add = ("children");
-   container.appendChild(box);
+  let numericalchoice = parseInt (choice, 10);
+  if (numericalchoice < 2 || numericalchoice > 100)
+  {
+    alert("Choice is invalid")
   }
-
+ 
+  else
+  {
+for (i = 0; i < numericalchoice * numericalchoice; i++)
+{
+   let container = document.getElementById("container");
+   container.style.gridTemplateColumns = `repeat(${numericalchoice}, 1fr)`;
+   container.style.gridTemplateRows = `repeat(${numericalchoice}, 1fr)`;
+   let box = document.createElement("div");
+   box.classList.add("children");
+   container.appendChild(box);
+   document.getElementById("start").disabled = true;
+} } 
 }
 
 
 
+function reset(){
+  document.querySelectorAll('.children').forEach(e => e.remove());
+  document.getElementById("start").disabled = false;
+}
 
+
+function changetoBlack(){
+  if (click = true){
+  let bambini = document.querySelectorAll(".children");
+for (i = 0; i < bambini.length; i++){
+  bambini[i].addEventListener("mouseover", (e) => {
+e.target.style.backgroundColor = "black";
+  })
+}
+}}
+
+function erase(){
+  if (click = true){
+  let bambini = document.querySelectorAll(".children");
+for (i = 0; i < bambini.length; i++){
+  bambini[i].addEventListener("mouseover", (e) => {
+e.target.style.backgroundColor = "white";
+  })
+}
+}}
+
+document.querySelector("body").addEventListener("click", (e) => {
+  click = !click;
+ console.log(e.target);
+ console.log(Boolean, click);
+});
 
